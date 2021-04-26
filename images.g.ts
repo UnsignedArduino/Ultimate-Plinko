b1ae5167a1a845f49c234b59c7a3d2e75f481a6e
Ckyiu <UnsignedArduino@outlook.com>
2021-04-26 17:23:52 -0400

    Make Plinko board

---
+++ images.g.ts
@@ -0,0 +1,38 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "clear_tile":return img`
+. . . . 
+. . . . 
+. . . . 
+. . . . 
+`;
+            case ";mKOJA=fPT]q":
+            case "pole":return img`
+. c c . 
+c c c c 
+c c c c 
+. c c . 
+`;
+            case "odQG^_YS0*eIr,td%$)?":
+            case "wall":return img`
+c c c c 
+c c c c 
+c c c c 
+c c c c 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
-
+function clear_tilemap () {
+    for (let row = 0; row <= tiles.tilemapRows() - 1; row++) {
+        for (let col = 0; col <= tiles.tilemapColumns() - 1; col++) {
+            tiles.setTileAt(tiles.getTileLocation(col, row), assets.image`clear_tile`)
+        }
+    }
+}
+function make_map () {
+    for (let row = 0; row <= tiles.tilemapRows() / 4 - 2; row++) {
+        for (let col = 0; col <= tiles.tilemapColumns() / 4 - 2; col++) {
+            if (row % 2 == 0) {
+                tiles.setTileAt(tiles.getTileLocation((col + 1) * 4 - 2, (row + 1) * 4 - 1), assets.image`pole`)
+            } else {
+                tiles.setTileAt(tiles.getTileLocation((col + 1) * 4 - 1 + 1, (row + 1) * 4 - 1), assets.image`pole`)
+            }
+        }
+    }
+    for (let col = 0; col <= tiles.tilemapColumns() - 1; col++) {
+        tiles.setTileAt(tiles.getTileLocation(col, tiles.tilemapRows() - 1), assets.image`wall`)
+        tiles.setWallAt(tiles.getTileLocation(col, tiles.tilemapRows() - 1), true)
+    }
+    for (let row = 0; row <= tiles.tilemapRows() - 1; row++) {
+        tiles.setTileAt(tiles.getTileLocation(0, row), assets.image`wall`)
+        tiles.setWallAt(tiles.getTileLocation(0, row), true)
+        tiles.setTileAt(tiles.getTileLocation(tiles.tilemapColumns() - 1, row), assets.image`wall`)
+        tiles.setWallAt(tiles.getTileLocation(tiles.tilemapColumns() - 1, row), true)
+    }
+}
+scene.setBackgroundColor(11)
+micromaps.createTilemap(micromaps.TileSize.Four, scene.screenWidth() / 4, scene.screenHeight() / 4)
+clear_tilemap()
+make_map()
